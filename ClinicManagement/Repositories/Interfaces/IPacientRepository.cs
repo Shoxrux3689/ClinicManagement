0f5fcadcc5797ebea9bb028a9b41086eacfa9cdc
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-26 17:08:28 +0500

    added implementation pacient repository

--- ClinicManagement/Repositories/Interfaces/IPacientRepository.cs
+++ ClinicManagement/Repositories/Interfaces/IPacientRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace ClinicManagement.Repositories.Interfaces;
+﻿using ClinicManagement.Entities;
+
+namespace ClinicManagement.Repositories.Interfaces;
 
 public interface IPacientRepository
 {
-    public ValueTask GetPacients();
-    public ValueTask GetPacient();
+    public ValueTask<List<Pacient>> GetPacients();
+    public ValueTask<Pacient?> GetPacientById(int Id);
+    public ValueTask<Pacient?> GetPacientByPhoneNumber(string phoneNumber);
+    public ValueTask<Pacient?> GetPacientByName(string name);
+    public ValueTask<bool> CreatePacient(Pacient pacient);
+    public ValueTask<bool> UpdatePacient(Pacient pacient);
+    public ValueTask<bool> DeletePacient(Pacient pacient);
 }
--- ClinicManagement/Repositories/PacientRepository.cs
+++ ClinicManagement/Repositories/PacientRepository.cs
@@ -1,8 +1,63 @@
-﻿using ClinicManagement.Repositories.Interfaces;
+﻿using ClinicManagement.Context;
+using ClinicManagement.Entities;
+using ClinicManagement.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace ClinicManagement.Repositories;
 
 public class PacientRepository : IPacientRepository
 {
+    private readonly AppDbContext _context;
 
+    public PacientRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask<bool> CreatePacient(Pacient pacient)
+    {
+        _context.Pacients.Add(pacient);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async ValueTask<bool> DeletePacient(Pacient pacient)
+    {
+        _context.Pacients.Update(pacient);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async ValueTask<Pacient?> GetPacientById(int Id)
+    {
+        var pacient = await _context.Pacients.FirstOrDefaultAsync(p => p.Id == Id);
+        return pacient;
+    }
+
+    public async ValueTask<Pacient?> GetPacientByName(string name)
+    {
+        var pacient = await _context.Pacients.FirstOrDefaultAsync(p => p.Name == name);
+        return pacient;
+    }
+
+    public async ValueTask<Pacient?> GetPacientByPhoneNumber(string phoneNumber)
+    {
+        var pacient = await _context.Pacients.FirstOrDefaultAsync(p => p.PhoneNumber == phoneNumber);
+        return pacient;
+    }
+
+    public async ValueTask<List<Pacient>> GetPacients()
+    {
+        var pacients = await _context.Pacients.ToListAsync();
+        return pacients;
+    }
+
+    public async ValueTask<bool> UpdatePacient(Pacient pacient)
+    {
+        _context.Pacients.Update(pacient);
+        await _context.SaveChangesAsync();
+        return true;
+    }
 }
