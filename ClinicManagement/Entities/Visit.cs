83a3eb475037c826b041c1b00c80ce37e14392b3
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-10-06 18:34:19 +0500

    a

--- ClinicManagement/Entities/Visit.cs
+++ ClinicManagement/Entities/Visit.cs
@@ -6,5 +6,7 @@ public class Visit
     public required DateTime VisitDate { get; set; }
     public long Price { get; set; }
     public string? Description { get; set; }
+    public Pacient? Pacient { get; set; }
+    public int PacientId { get; set; }
     public List<VisitProcedure>? VisitProcedures { get; set; }
 }
