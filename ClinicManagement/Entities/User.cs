f2e5f1ae613b10e0125e6f6e536c531cc1934e26
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-12-08 16:04:39 +0500

    sx

--- ClinicManagement/Entities/User.cs
+++ ClinicManagement/Entities/User.cs
@@ -7,4 +7,6 @@ public class User
     public required string PasswordHash { get; set; }
     public string? PhoneNumber { get; set; }
     public required string OrganizationName { get; set; }
+    public List<Procedure>? Procedures { get; set; }
+    public List<Pacient>? Pacients { get; set; }
 }
