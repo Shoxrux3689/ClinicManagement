a86850315d83ba9b8a1feaddc627ebdcd18c4837
Muhammadjon05 <muhammadjonmakhkamboyev@gmail.com>
2023-12-10 11:01:30 +0500

    Patient Repository has been completed

---
+++ Clinic.Services/Extensions/QueryableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Clinic.Services.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Clinic.Services.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<IEnumerable<TEntity>> ToPagedListAsync<TEntity>(this IQueryable<TEntity> source,
+        HttpContextHelper httpContextHelper, PaginationParams filterParams)
+    {
+        var content = JsonConvert.SerializeObject(
+            new PaginationMetaData(source.Count(), filterParams.AmountData, filterParams.PageNumber));
+
+        httpContextHelper.AddResponseToHeader("X-Pagination", content);
+
+        return await source.Skip(filterParams.AmountData * (filterParams.PageNumber - 1)).
+            Take(filterParams.AmountData).ToListAsync();
+    }
+    
+}
--- Clinic.Services/Repositories/PatientRepositories/PatientRepository.cs
+++ Clinic.Services/Repositories/PatientRepositories/PatientRepository.cs
@@ -2,9 +2,12 @@ using AutoMapper;
 using Clinic.Domain.Dto_s;
 using Clinic.Domain.Entities;
 using Clinic.Services.Exceptions;
+using Clinic.Services.Extensions;
 using Clinic.Services.Filters;
+using Clinic.Services.Pagination;
 using Clinic.Services.Repositories.Generic;
 using Clinic.ViewModel.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Clinic.Services.Repositories.PatientRepositories;
 
@@ -12,14 +15,18 @@ public class PatientRepository : IPatientRepository
 {
     private readonly IGenericRepository<Patient> _genericRepository;
     private readonly IMapper _mapper;
+    private readonly HttpContextHelper _httpContextHelper;
     private readonly IGenericRepository<Organization> _organizationRepository;
-    public PatientRepository(IGenericRepository<Patient> genericRepository, IGenericRepository<Organization> organizationRepository, IMapper mapper)
+
+    public PatientRepository(IGenericRepository<Patient> genericRepository,
+        IGenericRepository<Organization> organizationRepository, IMapper mapper, HttpContextHelper httpContextHelper)
     {
         _genericRepository = genericRepository;
         _organizationRepository = organizationRepository;
         _mapper = mapper;
+        _httpContextHelper = httpContextHelper;
     }
-    
+
     public async ValueTask<PatientModel> AddPatient(int organizationId, PatientDto patientDto)
     {
         var isOrganizationExist = await _organizationRepository.HasAnyAsync(c => c.Id == organizationId);
@@ -35,18 +42,71 @@ public class PatientRepository : IPatientRepository
         return _mapper.Map<PatientModel>(patient);
     }
 
-    public ValueTask<IEnumerable<PatientModel>> GetPatients(PatientFilter filter)
+    public async ValueTask<IEnumerable<PatientModel>> GetPatients(PatientFilter filter)
     {
-        throw new NotImplementedException();
+        var patients = _genericRepository.SelectAll();
+        if (filter.FirstName is not null)
+        {
+            patients = patients.Where(t => t.FirstName.ToLower()
+                .Contains(filter.FirstName.ToLower()));
+        }
+
+        if (filter.LastName is not null)
+        {
+            patients = patients.Where(t => t.LastName.ToLower()
+                .Contains(filter.LastName.ToLower()));
+        }
+
+        if (filter.PhoneNumber is not null)
+        {
+            patients = patients.Where(t => t.PhoneNumber.ToLower()
+                .Contains(filter.PhoneNumber.ToLower()));
+        }
+
+        if (filter.Gender is not null)
+        {
+            patients = patients.Where(t => t.PhoneNumber == filter.PhoneNumber);
+        }
+        if (filter.OrganizationId is not null)
+        {
+            patients = patients.Where(t => t.OrganizationId == filter.OrganizationId);
+        }
+        if(filter.CreatedDate is not null)
+        {
+            patients = patients.Where(t=>t.CreatedDate == filter.CreatedDate);
+        }
+        var patientsPages = patients.ToPagedListAsync(_httpContextHelper,filter).Result;
+        return patientsPages.Select(v => _mapper.Map<PatientModel>(v));
     }
 
-    public ValueTask<PatientModel> GetPatientById(int patientId)
+    public async ValueTask<PatientModel> GetPatientById(int organizationId,int patientId)
     {
-        throw new NotImplementedException();
+        var isOrganizationExist = await _organizationRepository.HasAnyAsync(c => c.Id == organizationId);
+        if (!isOrganizationExist)
+        {
+            throw new OrganizationIsNotExistsException(organizationId);
+        }
+        var patient = await _genericRepository.SelectFirstAsync(t => t.Id == patientId);
+        if (patient is null)
+        {
+            throw new PatientNotFoundException(patientId);
+        }
+        return _mapper.Map<PatientModel>(patient);
     }
 
-    public void DeletePatient(int patientId)
+    public void DeletePatient(int organizationId, int patientId)
     {
-        throw new NotImplementedException();
+        var isOrganizationExist = _organizationRepository.HasAnyAsync(c => c.Id == organizationId).Result;
+        if (!isOrganizationExist)
+        {
+            throw new OrganizationIsNotExistsException(organizationId);
+        }
+        var patient = _genericRepository.SelectFirstAsync(t => t.Id == patientId).Result;
+        if (patient is null)
+        {
+            throw new PatientNotFoundException(patientId);
+        }
+        _genericRepository.DeleteAsync(patient);
     }
+    
 }
