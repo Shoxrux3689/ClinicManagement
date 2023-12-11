91ecf8f68f25805ca8c3aaa88b980fbe25fb337a
Muhammadjon05 <muhammadjonmakhkamboyev@gmail.com>
2023-12-11 09:58:09 +0500

    Organization Controller added

--- Clinic.Services/Repositories/OrganizationRepository/IOrganizationRepository.cs
+++ Clinic.Services/Repositories/OrganizationRepository/IOrganizationRepository.cs
@@ -5,7 +5,7 @@ namespace Clinic.Services.Repositories.OrganizationRepository;
 
 public interface IOrganizationRepository
 {
-    ValueTask<OrganizationModel> AddOrganization(OrganizationDto organizationDto);
+    ValueTask<OrganizationModel> RegisterOrganization(OrganizationDto organizationDto);
     ValueTask<IEnumerable<OrganizationModel>?> GetOrganizations();
     ValueTask<OrganizationModel?> GetOrganizationById(int organizationId);
     ValueTask DeleteOrganization(int organizationId);
